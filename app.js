310cd15c9d4371d503e6ac86ff77fe44cd0b7f1a
Reinaldo Junior <juniorz@gmail.com>
2013-05-25 15:12:35 -0300

    Login with Facebook via JS

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var express = require('express')
 
 var app = express();
 
+var facebookGlobals = function(req, res, next){
+  res.locals.FACEBOOK_APP_ID = app.get('facebook app id');
+  next();
+};
+
 // all environments
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -20,9 +25,14 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
+  app.use(facebookGlobals);
   app.use(app.router);
   app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
+
+  //TODO: fallback to a file not checked in
+  app.set('facebook app id', process.env.FACEBOOK_APP_ID);
+  app.set('facebook app secret', process.env.FACEBOOK_APP_SECRET);
 });
 
 // development only
