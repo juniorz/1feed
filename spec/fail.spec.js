c026cfaa79f8db8f75636a9a9a29bd91f21dec93
Reinaldo Junior <juniorz@gmail.com>
2013-05-25 00:53:18 -0300

    jasmine-node
    it should fail

---
+++ spec/fail.spec.js
@@ -0,0 +1,9 @@
+describe("A suite", function() {
+  it("should pass", function() {
+    expect(true).toBe(true);
+  });
+
+  it("should fails", function() {
+    expect(false).toBe(true);
+  });
+});
