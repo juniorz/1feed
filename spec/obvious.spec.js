f09a8bfe18f0bdf8bd6986aab40b0c1040a22246
Reinaldo Junior <juniorz@gmail.com>
2013-05-25 01:07:39 -0300

    captain obvious

--- spec/obvious.spec.js
+++ spec/obvious.spec.js
@@ -2,8 +2,4 @@ describe("A suite", function() {
   it("should pass", function() {
     expect(true).toBe(true);
   });
-
-  it("should fails", function() {
-    expect(false).toBe(true);
-  });
 });
