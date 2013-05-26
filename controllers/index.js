c00ebd7d3b9063c9686a5bcdfe41bc9f017fc306
Reinaldo Junior <juniorz@gmail.com>
2013-05-26 00:34:03 -0300

    Using promises (via Q) to mitigate the callback-inception
    More info: http://documentup.com/kriskowal/q and http://promises-aplus.github.io/promises-spec/

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,22 @@
+var Q = require('q');
+
 exports.index = function(req, res){
-  req.models.user.find(function(err, users){
+
+  function render(users){
     res.render('index', {
-      users: users
+      users: users || []
     });
-  });
+  }
+
+  // Uncomment to see something bad happening
+  // Q.nfcall(req.models.user.find, {foo: "bar"})
+  Q.nfcall(req.models.user.find)
+  .then(render)
+  .fail(function(error){
+    //This is a clever way to define fallback
+    console.log('! Noooooooo.' + error);
+    render()
+  })
+  .done();
+
 };
